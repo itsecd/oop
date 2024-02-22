e7fa641afc6580e5f8926c40228a1f797151e3c3
yzkv <129549625+yzkv@users.noreply.github.com>
2024-02-22 08:47:01 +0400

    [base] Update practice2c-sfml

--- base/practice2c-sfml/main.cpp
+++ base/practice2c-sfml/main.cpp
@@ -1,4 +1,4 @@
-// TUTORIALS
+﻿// TUTORIALS
 // Getting started   SFML with the CMake Project Template   https://www.sfml-dev.org/tutorials/2.6/start-cmake.php
 // Window module     Opening and managing a SFML window     https://www.sfml-dev.org/tutorials/2.6/window-window.php
 // Window module     Events explained                       https://www.sfml-dev.org/tutorials/2.6/window-events.php
@@ -18,12 +18,12 @@ int main() {
   sf::RenderWindow window(sf::VideoMode(600u, 400u), "Practice 2C: SFML");
   window.setFramerateLimit(30);
 
+  sf::VertexArray lines(sf::PrimitiveType::LineStrip);
+  vector<sf::CircleShape> circles;
+
   vector<sf::Color> colors;
   colors.push_back(sf::Color::Red);
   colors.push_back(sf::Color::Blue);
-
-  vector<sf::CircleShape> circles;
-  sf::VertexArray lines(sf::PrimitiveType::LineStrip);
 
   sf::Clock clock;
 
@@ -31,16 +31,17 @@ int main() {
     sf::Event e;
     while (window.pollEvent(e)) {
       switch (e.type) {
-      case sf::Event::Closed:
+      case sf::Event::Closed: {
         window.close();
         break;
-      case sf::Event::MouseButtonPressed:
+      }
+      case sf::Event::MouseButtonPressed: {
         const float radius = 10;
 
-        auto pos = sf::Vector2f(e.mouseButton.x, e.mouseButton.y);
+        sf::Vector2f pos(e.mouseButton.x, e.mouseButton.y);
 
-        auto color_idx = static_cast<int>(clock.getElapsedTime().asSeconds()) % colors.size();
-        auto color = colors[color_idx];
+        size_t color_idx = static_cast<size_t>(clock.getElapsedTime().asSeconds()) % colors.size();
+        sf::Color color = colors[color_idx];
 
         sf::CircleShape circle(radius);
         circle.setPosition(pos - sf::Vector2f(radius, radius));
@@ -51,15 +52,16 @@ int main() {
 
         break;
       }
+      }
     }
 
     window.clear(sf::Color::White);
 
-    for (size_t i = 0; i < circles.size(); ++i) {
-      window.draw(circles[i]);
-    }
-
     window.draw(lines);
+
+    for (const auto& circle : circles) { // for (size_t i = 0; i < circles.size(); ++i) {
+      window.draw(circle);               //   window.draw(circles[i]);
+    }                                    // }
 
     window.display();
   }
